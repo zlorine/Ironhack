8178c4e327104b68ae8228dc236c546212b3dfc8
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2017-01-19 19:00:35 +0100

    first commit, adds models and database

---
+++ AuctionSite/db/migrate/20170119175820_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+	    t.text :title
+	    t.text :description
+	    t.datetime :deadline
+	    t.timestamps
+    end
+  end
+end
